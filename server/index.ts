fc5c7cf1810fb1cbb77b4036e0ea4de7ba3513f2
Alex Yu <alex@Alexs-MacBook-Pro-2.local>
2023-02-12 03:49:13 -0500

    complete endpoint

--- server/index.ts
+++ server/index.ts
@@ -2,19 +2,57 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import puppeteer from "puppeteer";
-import axios from "axios";
+import axios, { all } from "axios";
+import cohere from "cohere-ai";
 
 dotenv.config();
 
 const app: Express = express();
 app.use(cors());
+
 const port = process.env.PORT;
+const youtubeAPIKey = process.env.YOUTUBE_API_KEY;
+const cohereAPIKey = process.env.COHERE_API_KEY;
 
-const redditResults = async (searchQuery: string) => {};
+if (!youtubeAPIKey || !cohereAPIKey) process.exit(1);
 
-const youtubeResults = async (searchQuery: string) => {};
+cohere.init(cohereAPIKey);
 
-const googleResults = async (searchQuery: string) => {
+const redditURL =
+  "https://www.reddit.com/search.json?sort=relevance&type=link&limit=100&q=";
+const youtubeURL = `https://www.googleapis.com/youtube/v3/search?key=${youtubeAPIKey}&part=snippet&type=video&maxResults=2&q=`;
+
+const getRedditResults = async (searchQuery: string) => {
+  const response = await axios.get(redditURL + searchQuery);
+  const data = response.data.data.children;
+  const searchResults = data
+    .filter((item: any) => item.data.selftext !== "")
+    .map((item: any) => ({
+      title: item.data.title,
+      link: item.data.url,
+      description: item.data.selftext,
+      subreddit: item.data.subreddit_name_prefixed,
+      author: item.data.author,
+    }));
+  return searchResults.slice(0, 3);
+};
+
+const getYoutubeResults = async (searchQuery: string) => {
+  const response = await axios.get(youtubeURL + searchQuery);
+  const data = response.data.items;
+  const searchResults = data.map((item: any) => {
+    return {
+      title: item.snippet.title,
+      link: `https://www.youtube.com/watch?v=${item.id.videoId}`,
+      description: item.snippet.description,
+      thumbnail: item.snippet.thumbnails.default.url,
+      author: item.snippet.channelTitle,
+    };
+  });
+  return searchResults;
+};
+
+const getGoogleResults = async (searchQuery: string) => {
   const browser = await puppeteer.launch();
   const [page] = await browser.pages();
   await page.setRequestInterception(true);
@@ -58,19 +96,64 @@ const googleResults = async (searchQuery: string) => {
     });
   }
 
-  // const searchResults = await page.$$eval(".LC20lb", (els) => {
-  //   return els.map((e: any) => ({
-  //     title: e.innerText,
-  //     link: e.parentNode.href,
-  //   }));
-  // });
-
   await browser.close();
   return searchResults;
 };
 
-app.get("/watermelon", (req: Request, res: Response) => {
-  res.send("");
+const shuffle = (array: any[]) => {
+  let currentIndex = array.length,
+    randomIndex;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
+
+app.get("/watermelon", async (req: Request, res: Response) => {
+  if (!req.query.q) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const searchTerm = req.query.q as string;
+
+  const redditResults = await getRedditResults(encodeURIComponent(searchTerm));
+  const youtubeResults = await getYoutubeResults(
+    encodeURIComponent(`${searchTerm}|${searchTerm} review`)
+  );
+  const googleResults = await getGoogleResults(searchTerm);
+
+  const allDescriptions =
+    "" +
+    redditResults.map((item: any) => item.description).join("\n") +
+    "\n" +
+    youtubeResults.map((item: any) => item.description).join("\n") +
+    "\n" +
+    googleResults.map((item: any) => item.description).join("\n");
+
+  const cohereResponse = await cohere.generate({
+    model: "command-xlarge-nightly",
+    prompt: "Summarize the following:\n" + allDescriptions,
+    max_tokens: 150,
+    temperature: 0.9,
+    k: 0,
+    p: 0.75,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop_sequences: [],
+    return_likelihoods: "NONE",
+  });
+
+  res.send({
+    summary: cohereResponse.body.generations[0].text,
+    items: shuffle([...redditResults, ...youtubeResults, ...googleResults]),
+  });
 });
 
 app.listen(port, () => {
